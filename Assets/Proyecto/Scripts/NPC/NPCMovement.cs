742875be3c61fb71add6da205ac54d1b936df0e6
Alejandro Morales <alex31016@gmail.com>
2018-01-27 19:19:32 -0600

    Enemigos ya se alejan de ti.

--- Assets/Proyecto/Scripts/NPC/NPCMovement.cs
+++ Assets/Proyecto/Scripts/NPC/NPCMovement.cs
@@ -5,13 +5,32 @@ using UnityEngine.AI;
 
 public class NPCMovement : Movement {
 
+    public LayerMask playerLayer;
     private NavMeshAgent navMeshAgent;
+    private Vector3 direction;
+    //private 
+    
 
     protected override void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
+    private void Start() {
+        direction = Vector3.forward;
+        InvokeRepeating("CheckSurroundings", 0.1f, 1.0f);
+    }
+
     void FixedUpdate() {
-        navMeshAgent.Move(Vector3.forward);
+        navMeshAgent.Move(direction * Time.deltaTime * speed);
+    }
+
+    void CheckSurroundings() {
+        Collider[] players = Physics.OverlapSphere(GetComponent<Transform>().position, 5.0f, playerLayer);
+        for(int i=0; i<players.Length; i++) {
+            direction = players[i].GetComponent<Transform>().position - GetComponent<Transform>().position;
+            direction = direction.normalized*-1;
+            //navMeshAgent.SetDestination(players[i].GetComponent<Transform>().position*-1);
+        }
+        
     }
 }
