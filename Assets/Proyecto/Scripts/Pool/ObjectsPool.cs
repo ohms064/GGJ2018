7776c53e07057519cf57ce42696a944026ee6151
oms064 <otokonoko064@gmail.com>
2018-01-28 07:37:48 -0600

    El scripts genérico del pool.

---
+++ Assets/Proyecto/Scripts/Pool/ObjectsPool.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ObjectsPool<T> : MonoBehaviour where T : PoolObject {
+
+    private T PoolObject {
+        get {
+            return poolObjects[Random.Range( 0, poolObjects.Length )];
+        }
+    }
+    [AssetsOnly]
+    public T[] poolObjects;
+
+    [ValidateInput( "ValidatePoolSize" )]
+    public int poolSize = 1;
+    private T[] pool;
+    private int iterator = 0;
+
+#if UNITY_EDITOR
+    private bool ValidatePoolSize (int i) {
+        return i > 0;
+    }
+#endif
+
+
+    public IEnumerable<T> GetUnavailableObjects () {
+        for ( int i = 0; i < pool.Length; i++ ) {
+            if ( !pool[i].Available )
+                yield return pool[i];
+        }
+    }
+
+    public IEnumerable<T> GetAvailableObjects () {
+        for ( int i = 0; i < pool.Length; i++ ) {
+            if ( pool[i].Available )
+                yield return pool[i];
+        }
+    }
+
+    private void Awake () {
+        pool = new T[poolSize];
+        for ( int i = 0; i < poolSize; i++ ) {
+            pool[i] = Instantiate( PoolObject );
+            pool[i].gameObject.SetActive( false );
+        }
+    }
+
+    public bool RequestPoolObject (out T poolObject) {
+        if ( iterator >= pool.Length ) {
+            iterator = 0;
+        }
+        poolObject = pool[iterator];
+        iterator++;
+        return poolObject.Available;
+    }
+}
+
+public abstract class PoolObject : MonoBehaviour {
+    public virtual bool Available {
+        get {
+            return !gameObject.activeSelf;
+        }
+    }
+    public Vector2 position {
+        get {
+            return transform.position;
+        }
+    }
+
+    public abstract void Spawn (Vector2 position);
+
+    public virtual void Despawn () {
+        gameObject.SetActive( false );
+    }
+}
